953b6ac3cd252f5b418274b882931352c03cbd02
jasonho2 <31460284+jasonho2@users.noreply.github.com>
2025-01-31 18:05:35 -0800

    Fix initialize function to include buckets, add set method

--- lib/hashmap.rb
+++ lib/hashmap.rb
@@ -1,9 +1,10 @@
 class HashMap
-  attr_accessor :loadfactor, :capacity
+  #attr_accessor :loadfactor, :capacity
 
-  def initialize(loadfactor = nil, capacity = nil)
+  def initialize(loadfactor = 0.75, capacity = 16)
     @loadfactor = loadfactor
     @capacity = capacity
+    @buckets = Array.new(@capacity) { [] }
   end
 
   def hash(key)
@@ -13,6 +14,20 @@ class HashMap
     key.each_char { |char| hash_code = prime_number * hash_code + char.ord }
        
     hash_code
-  end 
+  end
+
+  def set(key, value)
+    index = hash(key) % @capacity
+    raise IndexError if index.negative? || index >= @buckets.length
+    bucket = @buckets[index]
+
+    bucket.each do |pair|
+      if pair[0] == key
+        pair[1] = value
+      end
+    end
+
+    bucket << [key, value]
+  end
 
 end
