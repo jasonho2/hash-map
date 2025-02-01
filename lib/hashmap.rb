4a8f20291a03f9d006ed4941e95866d7d4cce19c
jasonho2 <31460284+jasonho2@users.noreply.github.com>
2025-01-31 16:21:18 -0800

    Add hash(key) function from TOP lesson

--- lib/hashmap.rb
+++ lib/hashmap.rb
@@ -5,5 +5,14 @@ class HashMap
     @loadfactor = loadfactor
     @capacity = capacity
   end
-  
+
+  def hash(key)
+    hash_code = 0
+    prime_number = 31
+       
+    key.each_char { |char| hash_code = prime_number * hash_code + char.ord }
+       
+    hash_code
+  end 
+
 end
